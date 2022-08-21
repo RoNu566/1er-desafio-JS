1f617b4d107aa43730a13fbfccf7937503a52eb0
RoNu566 <rocion5666@gmail.com>
2022-08-21 20:31:14 -0300

    primer desafio entregable

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+
+// let username = prompt("ingresar nombre de usuario");
+//  let password = prompt(username + " " + "ingresa tu contraseña");
+//  console.log(password6)
+//  debugger
+
+//  if (username ==="MUS") {
+//      console.log("Bienvenidos", username);
+// } else {
+//     console.warn("advertenica", username)
+// }
+
+//  if (username === "MUS" && password === "98765") {
+//     alert("bienvenido", username)
+//  } else {
+//      alert("username y/o password es incorrecta")
+//  }
+
+debugger
+
+let cantidad = parseInt(prompt("Ingresa la cantidad de veces"));
+
+for (let i = 1; i <= cantidad; i++){
+    alert("Hola! x"+ " " + i);
+}
